e9c43e74753e80e7b42c200efb2ac121a202e184
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-11-17 11:44:59 -0500

    Initial Commit

---
+++ Pop In Kids Club/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pop In Kids Club
+//
+//  Created by Chris Thompson on 11/17/17.
+//  Copyright © 2017 Chris Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pop In Kids ClubUITests/Pop_In_Kids_ClubUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Pop_In_Kids_ClubUITests.swift
+//  Pop In Kids ClubUITests
+//
+//  Created by Chris Thompson on 11/17/17.
+//  Copyright © 2017 Chris Thompson. All rights reserved.
+//
+
+import XCTest
+
+class Pop_In_Kids_ClubUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
